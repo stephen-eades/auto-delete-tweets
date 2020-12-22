dc88dc154dcbc53c7e18757d39801291e9ff8b0f
stephen.eades <stephen.a.eades@gmail.com>
2020-12-22 15:26:36 -0500

    fixed cron job

--- clock.py
+++ clock.py
@@ -4,7 +4,7 @@ import delete
 sched = BlockingScheduler()
 
 # This job runs every day at 4am
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=1530)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour='15', minute="30" timezone='America/New_York')
 def scheduled_job():
     delete.delete_old_tweets()
 
